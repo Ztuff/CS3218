002a1f3e7c89c59cf22869f396da7dc194af69dc
Reem Razak <reemrazak@gmail.com>
2016-04-13 21:55:19 +0800

    For the recording screen: created fragment for video component and integrated view with compass

---
+++ app/src/main/java/sg/edu/nus/cs3218project/RecordFragmentActivity.java
@@ -0,0 +1,44 @@
+package sg.edu.nus.cs3218project;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+/**
+ * Created by Reem on 2016-04-13.
+ */
+public class RecordFragmentActivity extends FragmentActivity {
+    FragmentManager fragmentManager = getFragmentManager();
+    FragmentTransaction fragmentTransaction;
+
+    @Override
+    public FragmentManager getFragmentManager() {
+        return super.getFragmentManager();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recordfragment);
+
+
+        CameraActivity textFragment = new CameraActivity();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.video, textFragment);
+        fragmentTransaction.commit();
+
+
+        CompassFragment compassFragment = new CompassFragment();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+
+        fragmentTransaction.add(R.id.compass, compassFragment);
+        fragmentTransaction.commit();
+
+
+
+    }
+}
