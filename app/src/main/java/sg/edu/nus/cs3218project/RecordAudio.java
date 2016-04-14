4e6aaeb2173572fef6ff138a6d065c613a238216
Reem Razak <reemrazak@gmail.com>
2016-04-14 22:58:47 +0800

    working with audio sampling

--- app/src/main/java/sg/edu/nus/cs3218project/RecordAudio.java
+++ app/src/main/java/sg/edu/nus/cs3218project/RecordAudio.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.cs3218project;
 
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.util.Log;
 
@@ -14,11 +15,21 @@ public class RecordAudio{
     private File outputDir = CamcorderView.outputDir;
     public static String outputFilename = "audio.mp4";
     public static long startTime;
+    private static final int  FS = 16000;     // sampling frequency
+    public AudioRecord audioRecord;
+    private int               audioEncoding = 2;
+    private int               nChannels = 16;
+    private Thread            recordingThread;
+    public  static short[]  buffer;
+    public  static int      bufferSize;
+    private long startTimeStamp;
+    public static long endTimeStamp;
 
     public RecordAudio(){
         mRecorder = new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+
         File outputFile = new File(outputDir, outputFilename);
         mRecorder.setOutputFile(outputFile.getAbsolutePath());
         mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
@@ -33,10 +44,64 @@ public class RecordAudio{
     }
 
     public void start(){
+
+        try {
+            audioRecord = new AudioRecord(1, FS, nChannels, audioEncoding, AudioRecord.getMinBufferSize(FS, nChannels, audioEncoding));
+        }
+        catch (Exception e) {
+            Log.d("Error in Init() ", e.getMessage());
+        }
+
+        bufferSize = AudioRecord.getMinBufferSize(FS, nChannels, audioEncoding);
+        buffer = new short[bufferSize];
+
         mRecorder.start();
+        audioRecord.startRecording();
+
+
+
+        recordingThread = new Thread()
+        {
+            public void run()
+            {
+                boolean firstFlag = true;
+                int firstAmpSum = 0;
+                int lastAmpSum;
+
+                while (true) {
+                    audioRecord.read(buffer, 0, bufferSize);
+                    if(firstFlag){
+                        for(int i = 0; i < bufferSize; i++){
+                            firstAmpSum += Math.abs(buffer[i]);
+
+                        }
+                        firstAmpSum = firstAmpSum/bufferSize;
+                        firstFlag = false;
+                    }
+                    else{
+                        lastAmpSum = 0;
+                        for(int i = 0; i < bufferSize; i++){
+                            lastAmpSum += Math.abs(buffer[i]);
+
+                        }
+                        lastAmpSum = lastAmpSum/bufferSize;
+
+                        if(lastAmpSum > firstAmpSum * 10){
+                            endTimeStamp = startTimeStamp - System.currentTimeMillis();
+                        }
+                    }
+                }
+            }
+        };
+        recordingThread.start();
+        startTimeStamp = System.currentTimeMillis();
+
     }
 
     public void stop() {
         mRecorder.reset();
+        recordingThread.stop();
+        audioRecord.stop();
+        audioRecord.release();
     }
 }
